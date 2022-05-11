927628c0ceaf86272540aa184331ce51d05aae5b
Jiron <59139789+Jiron@users.noreply.github.com>
2022-05-11 14:26:36 +0200

    feat: fixed image not loading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const { Intents } = require('discord.js');
 const config = require('./config.json');
+const https = require('https');
 
 const client = new Discord.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
@@ -13,15 +14,26 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', (member) => {
-    var user = member.user;
-    var imageURL = `https://api.discorddevtools.xyz/welcome-image-generator/generate.png?username=${user.username}&discriminator=${user.discriminator}&image=${user.avatarURL()}&usernameColor=fff&discriminatorColor=fff&title=Welcome+to+the+server!&titleColor=a9a9a9&textColor=a9a9a9&borderColor=a9a9a9&background=https://cdn.wallpapersafari.com/97/1/kSG1Bj.jpg`;
-    const embed = new Discord.MessageEmbed()
-    .setTitle('Welcome!')
-    .setImage(imageURL)
-    .setColor('BLURPLE');
+  var imageURL = `https://api.discorddevtools.xyz/welcome-image-generator/generate.png?username=${user.username}&discriminator=${user.discriminator}&image=${user.avatarURL()}&usernameColor=fff&discriminatorColor=fff&title=Welcome+to+the+server!&titleColor=a9a9a9&textColor=a9a9a9&borderColor=a9a9a9&background=https://cdn.wallpapersafari.com/97/1/kSG1Bj.jpg`;
+  https.get(imageURL, (resp) => {
+    resp.on('data', () => {
+      return;
+    });
+    // Await image to load. Quite necessary till the api gets improvement  
+    resp.on('end', () => {
+      var user = member.user;
+      const embed = new Discord.MessageEmbed()
+      .setTitle('Welcome!')
+      .setImage(imageURL)
+      .setColor('BLURPLE');
 
-    client.channels.cache.get(config.channelID).send(`<@${user.id}>`);
-    client.channels.cache.get(config.channelID).send({embeds: [embed]});
+      client.channels.cache.get(config.channelID).send(`<@${user.id}>`);
+      client.channels.cache.get(config.channelID).send({embeds: [embed]});
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
 });
 
 client.login(config.token);
